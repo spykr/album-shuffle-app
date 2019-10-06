977e0f8729007a41ea7867589ffa1d6f0e45f475
Darcy Glennen <spykr7@gmail.com>
2019-10-06 23:09:13 +1100

    Add Serverless config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 module.exports = {
+  target: "serverless",
   webpack(config, options) {
     config.resolve.alias["@"] = path.resolve(__dirname, "./src");
     return config;
